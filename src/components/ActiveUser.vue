b337ff2f49cfab15b2233918f66a5f85e595b1e6
Baia <bmais2018@agruni.edu.ge>
2022-12-30 01:06:02 +0400

    assignment 6

---
+++ src/components/ActiveUser.vue
@@ -0,0 +1,37 @@
+<template>
+	<section v-show="username && userAge">
+		<h2>{{ username }} is {{ userAge }} yo</h2>
+	</section>
+</template>
+
+<script>
+export default {
+	props: {
+		username: {
+			type: String,
+			required: true,
+		},
+		userAge: {
+			type: Number,
+			required: true,
+		},
+	},
+};
+</script>
+
+
+<style>
+h3 {
+	text-align: center;
+	width: 100%;
+	background: yellow;
+}
+
+section {
+	display: flex;
+	width: 100%;
+	gap: 1rem;
+	padding: 1rem;
+	border: 3px dashed #7d7de0;
+}
+</style>
